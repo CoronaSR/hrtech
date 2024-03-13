eccb662771b0f5a0c7c51a9522e129eb161e8803
EnriqueCSI <enrique.sandoval@csi01.com>
2024-03-12 18:19:33 -0600

    EntityFramework Usuarios y Empleados

--- HR Tech/Models/Solicitud.cs
+++ HR Tech/Models/Solicitud.cs
@@ -5,7 +5,6 @@ namespace HR_Tech.Models {
     public class Solicitud {
         [Key]
         public int IdSolicitud { get; set; }
-        [ForeignKey(nameof(IdSolicitud))]
         public int IdEmpleado { get; set; }
         public required string Descripcion { get; set; }
         public int DiasSolicitados { get; set; }
